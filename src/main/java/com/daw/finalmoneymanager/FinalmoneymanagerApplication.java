001685fa64c8309d995026771e8397fafc545394
Mario Ascaso <ascasovicentemario@gmail.com>
2025-10-29 20:09:20 +0100

    second commit

---
+++ src/main/java/com/daw/finalmoneymanager/FinalmoneymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.daw.finalmoneymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalmoneymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalmoneymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daw/finalmoneymanager/FinalmoneymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.finalmoneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalmoneymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
