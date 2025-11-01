b27b290579e6e90f4b59bc33f626f7e3cc5df142
Mario Ascaso <ascasovicentemario@gmail.com>
2025-11-01 12:19:27 +0100

    added improvements

--- src/main/java/com/daw/finalmoneymanager/listmovements/domain/ListMovementsUseCase.java
+++ src/main/java/com/daw/finalmoneymanager/listmovements/domain/ListMovementsUseCase.java
@@ -2,20 +2,37 @@ package com.daw.finalmoneymanager.listmovements.domain;
 
 import com.daw.finalmoneymanager.shared.domain.Movement;
 import com.daw.finalmoneymanager.shared.domain.MovementRepository;
+import com.daw.finalmoneymanager.shared.domain.TypeMovement;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 public class ListMovementsUseCase {
 
-    private final MovementRepository movementRepository;
-
     public ListMovementsUseCase(MovementRepository movementRepository) {
         this.movementRepository = movementRepository;
     }
 
-    public List<Movement> execute() {
-        return movementRepository.findAll();
+    private final MovementRepository movementRepository;
+
+    public MovementSummary execute() {
+
+        List<Movement> movements = movementRepository.findAllByOrderByDateDesc();
+
+        BigDecimal totalIngresos = movements.stream()
+                .filter(mov -> mov.getType() == TypeMovement.INCOME)
+                .map(Movement::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal totalGastos = movements.stream()
+                .filter(mov -> mov.getType() == TypeMovement.EXPENSES)
+                .map(Movement::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal balanceTotal = totalIngresos.subtract(totalGastos);
+
+        return new MovementSummary(movements, totalIngresos, totalGastos, balanceTotal);
     }
 }
